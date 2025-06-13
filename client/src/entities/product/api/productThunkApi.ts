d30f11c052b3c902498d41b85cf311f925737d1a
radicalHighway <igor.papin178@gmail.com>
2025-06-13 14:03:59 +0300

    productSlice fix

--- client/src/entities/product/api/productThunkApi.ts
+++ client/src/entities/product/api/productThunkApi.ts
@@ -1,11 +1,15 @@
 // src/entities/user/api/userThunkApi.ts
-import { PRODUCTS_THUNKS_TYPES } from '@/shared/enums/productThunkTypes';
 import { PRODUCTS_API_ROUTES } from '@/shared/enums/productApiRoutes';
+import { PRODUCTS_THUNKS_TYPES } from '@/shared/enums/productThunkTypes';
 import { axiosInstance } from '@/shared/lib/axiosInstance';
 import type { IApiResponseReject, IApiResponseSuccess } from '@/shared/types';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { AxiosError } from 'axios';
-import type { ArrayProductsType, IProduct, IRawProductData } from '../model/types';
+import type {
+  ArrayProductsType,
+  IProduct,
+  IRawProductData,
+} from '../model/types';
 
 // Получение всех продуктов
 export const getProducts = createAsyncThunk<
@@ -46,35 +50,41 @@ export const createProduct = createAsyncThunk<
   IApiResponseSuccess<IProduct>,
   IRawProductData,
   { rejectValue: IApiResponseReject }
->(PRODUCTS_THUNKS_TYPES.CREATE_PRODUCT, async (newProduct, { rejectWithValue }) => {
-  try {
-    const { data } = await axiosInstance.post<IApiResponseSuccess<ITask>>(
-      `${PRODUCTS_API_ROUTES.PRODUCTS}`,
-      newProduct
-    );
-    return data;
-  } catch (error) {
-    const err = error as AxiosError<IApiResponseReject>;
-    return rejectWithValue(err.response!.data);
+>(
+  PRODUCTS_THUNKS_TYPES.CREATE_PRODUCT,
+  async (newProduct, { rejectWithValue }) => {
+    try {
+      const { data } = await axiosInstance.post<IApiResponseSuccess<IProduct>>(
+        `${PRODUCTS_API_ROUTES.PRODUCTS}`,
+        newProduct
+      );
+      return data;
+    } catch (error) {
+      const err = error as AxiosError<IApiResponseReject>;
+      return rejectWithValue(err.response!.data);
+    }
   }
-});
+);
 
 // удаление продукта
 export const deleteProductById = createAsyncThunk<
   IApiResponseSuccess<IProduct>,
   number,
   { rejectValue: IApiResponseReject }
->(PRODUCTS_THUNKS_TYPES.DELETE_PRODUCT_BY_ID, async (id, { rejectWithValue }) => {
-  try {
-    const { data } = await axiosInstance.delete<IApiResponseSuccess<IProduct>>(
-      `${PRODUCTS_API_ROUTES.PRODUCTS}/${id}`
-    );
-    return data;
-  } catch (error) {
-    const err = error as AxiosError<IApiResponseReject>;
-    return rejectWithValue(err.response!.data);
+>(
+  PRODUCTS_THUNKS_TYPES.DELETE_PRODUCT_BY_ID,
+  async (id, { rejectWithValue }) => {
+    try {
+      const { data } = await axiosInstance.delete<
+        IApiResponseSuccess<IProduct>
+      >(`${PRODUCTS_API_ROUTES.PRODUCTS}/${id}`);
+      return data;
+    } catch (error) {
+      const err = error as AxiosError<IApiResponseReject>;
+      return rejectWithValue(err.response!.data);
+    }
   }
-});
+);
 
 // Обновление продукта
 export const updateProductById = createAsyncThunk<
@@ -85,7 +95,7 @@ export const updateProductById = createAsyncThunk<
   PRODUCTS_THUNKS_TYPES.UPDATE_PRODUCT_BY_ID,
   async ({ id, productData }, { rejectWithValue }) => {
     try {
-      const { data } = await axiosInstance.put<IApiResponseSuccess<ITask>>(
+      const { data } = await axiosInstance.put<IApiResponseSuccess<IProduct>>(
         `${PRODUCTS_API_ROUTES.PRODUCTS}/${id}`,
         productData
       );
