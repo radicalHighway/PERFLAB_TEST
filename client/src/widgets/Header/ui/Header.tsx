869d22e3540637c521e4bbbd1c40bb6e1bc3e031
radicalHighway <igor.papin178@gmail.com>
2025-06-18 17:24:59 +0300

    fix header

--- client/src/widgets/Header/ui/Header.tsx
+++ client/src/widgets/Header/ui/Header.tsx
@@ -1,18 +1,37 @@
 import { CLIENT_ROUTES } from '@/shared/enums/clientRoutes';
 import { Cart } from '@/widgets/Cart/ui/CartItem';
-import { Link } from 'react-router';
+import { Link, useLocation } from 'react-router';
 import styles from './styles.module.css';
 
 export const Header = () => {
+  const location = useLocation();
   return (
     <header className={styles.header}>
       <nav className={styles.nav}>
-        <Link to={CLIENT_ROUTES.HOME} className={styles.logo}>
-          Главная
-        </Link>
-        <div className={styles.categories}>
-          <Link to={CLIENT_ROUTES.PRODUCTS}>Продукты</Link>
-
+        <div className={styles.logo}>
+          <Link to={CLIENT_ROUTES.HOME} className={styles.logoText}>
+            Магазин
+          </Link>
+        </div>
+        <div className={styles.menu}>
+          <Link
+            to={CLIENT_ROUTES.HOME}
+            className={
+              location.pathname === CLIENT_ROUTES.HOME
+                ? styles.active
+                : styles.link
+            }>
+            Главная
+          </Link>
+          <Link
+            to={CLIENT_ROUTES.PRODUCTS}
+            className={
+              location.pathname.startsWith(CLIENT_ROUTES.PRODUCTS)
+                ? styles.active
+                : styles.link
+            }>
+            Продукты
+          </Link>
         </div>
         <div className={styles.cartContainer}>
           <Cart />
